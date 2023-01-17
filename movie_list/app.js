00e00c81731830167d6103eeb117970794d2ef80
TimWang95 <tgm761@gmail.com>
2023-01-17 17:34:16 +0800

    movie_list

---
+++ movie_list/app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const exphbs = require('express-handlebars') // 模組 
+const movieList = require('./movies.json')  // 檔案 的路徑 ./ 意指  同個資料夾
+const app = express()
+const port = 3000
+
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  // past the movie data into 'index' partial template
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const movies = movieList.results.filter(movie => {
+    return movie.title.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { movies: movies, keyword: keyword })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  // console.log('req.params.movie_id', req.params.movie_id)
+  const movie = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', { movie: movie })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
